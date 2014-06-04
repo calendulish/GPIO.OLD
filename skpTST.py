f06d2f5b6a940132e184532785da1a1972eea50b
Lara Maia <lara@craft.net.br>
2014-06-04 15:24:15 -0300

    Working skpTST.py

--- skpTST.py
+++ skpTST.py
@@ -5,13 +5,21 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 
 SPEAKER = 17
+
 GPIO.setup(SPEAKER,GPIO.OUT)
+
+pwmSPEAKER = GPIO.PWM(SPEAKER, 1)
+pwmSPEAKER.start(5)
 
 try:
     while True:
-        GPIO.output(SPEAKER,True)
-        sleep(0.25)
-        GPIO.output(SPEAKER,False)
+        for freq in range(500,1000, 1):
+            pwmSPEAKER.ChangeFrequency(freq)
+            sleep(0.001)
+        for freq in range(1000, 500, -1):
+            pwmSPEAKER.ChangeFrequency(freq)
+            sleep(0.001)
 except KeyboardInterrupt:
+    pwmSPEAKER.stop()
     GPIO.cleanup()
 exit()
