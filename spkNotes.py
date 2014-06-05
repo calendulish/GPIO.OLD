fc6672fb6b3bfc043f0cfa173d41685550bc947f
Lara Maia <lara@craft.net.br>
2014-06-05 18:30:01 -0300

    More sound tests

---
+++ spkNotes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+SPEAKER = 17
+GPIO.setup(SPEAKER, GPIO.OUT)
+
+pwmSPEAKER = GPIO.PWM(SPEAKER, 440)
+pwmSPEAKER.start(10)
+
+# Escala da 5° oitava
+C = 523.25
+D = 587.33
+E = 659.25
+F = 698.46
+G = 783.99
+A = 880.00
+B = 987.77
+
+C6 = 1046.50
+
+for freq in [ C, D, E, F, G, A, B, C6 ]:
+    pwmSPEAKER.ChangeFrequency(freq)
+    sleep(0.5)
+
+pwmSPEAKER.stop()
+GPIO.cleanup()
+exit(0)
---
+++ spkNotes2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import wiringpi2
+from time import sleep
+
+wiringpi2.wiringPiSetupGpio()
+
+SPEAKER = 17
+
+# Escala da 5° oitava
+C = 523
+D = 587
+E = 659
+F = 698
+G = 783
+A = 880
+B = 987
+
+C6 = 1046
+
+wiringpi2.softToneCreate(SPEAKER)
+
+for freq in [ C, D, E, F, G, A, B, C6 ]:
+    wiringpi2.softToneWrite(SPEAKER, freq)
+    sleep(0.5)
+
+wiringpi2.digitalWrite(SPEAKER, 0)
+wiringpi2.pinMode(SPEAKER, 0)
+exit(0)
