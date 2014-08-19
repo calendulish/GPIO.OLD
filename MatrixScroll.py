ab091eb9439b14e4262af4246a39750d11c3433e
Lara Maia <lara@craft.net.br>
2014-08-19 01:53:53 -0300

    Make lists start at index 0

--- MatrixScroll.py
+++ MatrixScroll.py
@@ -5,20 +5,19 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-# --------- > p0, p1, p2, p3, p4, p5, p6, p7
-pins1 = [ -1, 17, 18, 27, 22, 23, 24, 25, 4 ]
+# ----- > p0, p1, p2, p3, p4, p5, p6, p7
+pins1 = [ 17, 18, 27, 22, 23, 24, 25, 4 ]
 
-# --------- > ce1, ce0, sclk, miso, mosi, rxd, txd, scl
-pins2 = [ -1,  7,   8,   11,   9,    10,   15,  14,  3 ]
+# ----- > ce1, ce0, sclk, miso, mosi, rxd, txd, scl
+pins2 = [  7,   8,   11,   9,    10,   15,  14,  3 ]
 
-# --------- >   p0    17  p5    24  scl   3   p3    22  ce1   7   txd   14  ce0   8   mosi  10
-rows    = [ -1, pins1[1], pins1[6], pins2[8], pins1[4], pins2[1], pins2[7], pins2[2], pins2[5] ]
+# ----- >   p0    17  p5    24  scl   3   p3    22  ce1   7   txd   14  ce0   8   mosi  10
+rows    = [ pins1[0], pins1[5], pins2[7], pins1[3], pins2[0], pins2[6], pins2[1], pins2[4] ]
 
-# --------- >   p4    23  sclk  11  miso  9   p1    18  rxd   15  p2    27  p6    25  p7    4
-columns = [ -1, pins1[5], pins2[3], pins2[4], pins1[2], pins2[6], pins1[3], pins1[7], pins1[8] ]
+# ----- >   p4    23  sclk  11  miso  9   p1    18  rxd   15  p2    27  p6    25  p7    4
+columns = [ pins1[4], pins2[2], pins2[3], pins1[1], pins2[5], pins1[2], pins1[6], pins1[7] ]
 
-
-for i in range(1, 9):
+for i in range(0, 8):
     GPIO.setup(rows[i], GPIO.OUT)
     GPIO.setup(columns[i], GPIO.OUT)
     GPIO.output(rows[i], GPIO.LOW)
@@ -29,7 +28,7 @@ def _exit(status):
     exit(status)
 
 def led(column, row, status):
-    for col in range(1, 9):
+    for col in range(0, 8):
         if col != column:
             GPIO.output(columns[col], status)
     GPIO.output(rows[row], status)
@@ -45,20 +44,20 @@ try:
             i=8-i
             l=0
             while l < 5:
-                draw(3+i, 1)
-                draw(2+i, 2)
-                draw(1+i, 3)
+                draw(2+i, 0)
+                draw(1+i, 1)
+                draw(0+i, 2)
+                draw(0+i, 3)
                 draw(1+i, 4)
                 draw(2+i, 5)
                 draw(3+i, 6)
-                draw(4+i, 7)
-                draw(5+i, 6)
-                draw(6+i, 5)
-                draw(7+i, 4)
-                draw(7+i, 3)
+                draw(4+i, 5)
+                draw(5+i, 4)
+                draw(6+i, 3)
                 draw(6+i, 2)
                 draw(5+i, 1)
-                draw(4+i, 2)
+                draw(4+i, 0)
+                draw(3+i, 1)
                 sleep(0.001)
                 l+=1
 except KeyboardInterrupt:
--- MatrixTest.py
+++ MatrixTest.py
@@ -5,20 +5,20 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-# --------- > p0, p1, p2, p3, p4, p5, p6, p7
-pins1 = [ -1, 17, 18, 27, 22, 23, 24, 25, 4 ]
+# ----- > p0, p1, p2, p3, p4, p5, p6, p7
+pins1 = [ 17, 18, 27, 22, 23, 24, 25, 4 ]
 
-# --------- > ce1, ce0, sclk, miso, mosi, rxd, txd, scl
-pins2 = [ -1,  7,   8,   11,   9,    10,   15,  14,  3 ]
+# ----- > ce1, ce0, sclk, miso, mosi, rxd, txd, scl
+pins2 = [  7,   8,   11,   9,    10,   15,  14,  3 ]
 
-# --------- >   p0    17  p5    24  scl   3   p3    22  ce1   7   txd   14  ce0   8   mosi  10
-rows    = [ -1, pins1[1], pins1[6], pins2[8], pins1[4], pins2[1], pins2[7], pins2[2], pins2[5] ]
+# ----- >   p0    17  p5    24  scl   3   p3    22  ce1   7   txd   14  ce0   8   mosi  10
+rows    = [ pins1[0], pins1[5], pins2[7], pins1[3], pins2[0], pins2[6], pins2[1], pins2[4] ]
 
-# --------- >   p4    23  sclk  11  miso  9   p1    18  rxd   15  p2    27  p6    25  p7    4
-columns = [ -1, pins1[5], pins2[3], pins2[4], pins1[2], pins2[6], pins1[3], pins1[7], pins1[8] ]
+# ----- >   p4    23  sclk  11  miso  9   p1    18  rxd   15  p2    27  p6    25  p7    4
+columns = [ pins1[4], pins2[2], pins2[3], pins1[1], pins2[5], pins1[2], pins1[6], pins1[7] ]
 
 
-for i in range(1, 9):
+for i in range(0, 8):
     GPIO.setup(rows[i], GPIO.OUT)
     GPIO.setup(columns[i], GPIO.OUT)
     GPIO.output(rows[i], GPIO.LOW)
@@ -29,7 +29,7 @@ def _exit(status):
     exit(status)
 
 def led(column, row, status):
-    for col in range(1, 9):
+    for col in range(0, 8):
         if col != column:
             GPIO.output(columns[col], status)
     GPIO.output(rows[row], status)
@@ -41,20 +41,20 @@ def draw(column, row, delay=0.001):
 
 try:
     while True:
-        draw(3, 1)
-        draw(2, 2)
-        draw(1, 3)
+        draw(2, 0)
+        draw(1, 1)
+        draw(0, 2)
+        draw(0, 3)
         draw(1, 4)
         draw(2, 5)
         draw(3, 6)
-        draw(4, 7)
-        draw(5, 6)
-        draw(6, 5)
-        draw(7, 4)
-        draw(7, 3)
+        draw(4, 5)
+        draw(5, 4)
+        draw(6, 3)
         draw(6, 2)
         draw(5, 1)
-        draw(4, 2)
+        draw(4, 0)
+        draw(3, 1)
 
 except KeyboardInterrupt:
 	_exit(0)
