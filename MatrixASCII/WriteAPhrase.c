cec192cb3dfa9ce59e5397d09a3ef3eb221d031a
Lara Maia <lara@craft.net.br>
2014-09-02 10:19:00 -0300

    Be more repetitive and cool

--- MatrixASCII/WriteAPhrase.c
+++ MatrixASCII/WriteAPhrase.c
@@ -13,92 +13,101 @@ int main(int argc, char **argv)
     char line[256];
     char *character = line;
 
-    printf("Digite uma frase: ");
-    if (fgets(line, sizeof(line), stdin) != NULL)
+    for(;;)
     {
-        for(int count = 0; count < strlen(character)-1; count++)
+        if(STOP)
         {
-            switch(tolower(character[count])) {
-                case 'a':
-                    scroll(a, 3, 0);
-                    break;
-                case 'b':
-                    scroll(b, 3, 0);
-                    break;
-                case 'c':
-                    scroll(c, 4, 0);
-                    break;
-                case 'd':
-                    scroll(d, 3, 0);
-                    break;
-                case 'e':
-                    scroll(e, 3, 0);
-                    break;
-                case 'f':
-                    scroll(f, 4, 0);
-                    break;
-                case 'g':
-                    scroll(g, 4, 0);
-                    break;
-                case 'h':
-                    scroll(h, 4, 0);
-                    break;
-                case 'i':
-                    scroll(i, 9, -3);
-                    break;
-                case 'j':
-                    scroll(j, 4, 0);
-                    break;
-                case 'k':
-                    scroll(k, 4, 0);
-                    break;
-                case 'l':
-                    scroll(l, 5, 0);
-                    break;
-                case 'm':
-                    scroll(m, 3, 0);
-                    break;
-                case 'n':
-                    scroll(n, 3, 0);
-                    break;
-                case 'o':
-                    scroll(o, 3, 0);
-                    break;
-                case 'p':
-                    scroll(p, 4, 0);
-                    break;
-                case 'q':
-                    scroll(q, 3, 0);
-                    break;
-                case 'r':
-                    scroll(r, 3, 0);
-                    break;
-                case 's':
-                    scroll(s, 4, 0);
-                    break;
-                case 't':
-                    scroll(t, 5, 0);
-                    break;
-                case 'u':
-                    scroll(u, 4, 0);
-                    break;
-                case 'v':
-                    scroll(v, 5, 0);
-                    break;
-                case 'w':
-                    scroll(w, 4, 0);
-                    break;
-                case 'x':
-                    scroll(x, 4, 0);
-                    break;
-                case 'y':
-                    scroll(y, 5, 0);
-                    break;
-                case 'z':
-                    scroll(z, 3, 0);
-                    break;
-            }
+            clean();
+            exit(0);
         }
+
+        printf("Digite uma frase: ");
+        if (fgets(line, sizeof(line), stdin) != NULL)
+        {
+            for(int count = 0; count < strlen(character)-1; count++)
+            {
+                switch(tolower(character[count])) {
+                    case 'a':
+                        scroll(a, 3, 0);
+                        break;
+                    case 'b':
+                        scroll(b, 3, 0);
+                        break;
+                    case 'c':
+                        scroll(c, 4, 0);
+                        break;
+                    case 'd':
+                        scroll(d, 3, 0);
+                        break;
+                    case 'e':
+                        scroll(e, 3, 0);
+                        break;
+                    case 'f':
+                        scroll(f, 4, 0);
+                        break;
+                    case 'g':
+                        scroll(g, 4, 0);
+                        break;
+                    case 'h':
+                        scroll(h, 4, 0);
+                        break;
+                    case 'i':
+                        scroll(i, 9, -3);
+                        break;
+                    case 'j':
+                        scroll(j, 4, 0);
+                        break;
+                    case 'k':
+                        scroll(k, 4, 0);
+                        break;
+                    case 'l':
+                        scroll(l, 5, 0);
+                        break;
+                    case 'm':
+                        scroll(m, 3, 0);
+                        break;
+                    case 'n':
+                        scroll(n, 3, 0);
+                        break;
+                    case 'o':
+                        scroll(o, 3, 0);
+                        break;
+                    case 'p':
+                        scroll(p, 4, 0);
+                        break;
+                    case 'q':
+                        scroll(q, 3, 0);
+                        break;
+                    case 'r':
+                        scroll(r, 3, 0);
+                        break;
+                    case 's':
+                        scroll(s, 4, 0);
+                        break;
+                    case 't':
+                        scroll(t, 5, 0);
+                        break;
+                    case 'u':
+                        scroll(u, 4, 0);
+                        break;
+                    case 'v':
+                        scroll(v, 5, 0);
+                        break;
+                    case 'w':
+                        scroll(w, 4, 0);
+                        break;
+                    case 'x':
+                        scroll(x, 4, 0);
+                        break;
+                    case 'y':
+                        scroll(y, 5, 0);
+                        break;
+                    case 'z':
+                        scroll(z, 3, 0);
+                        break;
+                }
+            }
+            }
     }
     exit(0);
 }
