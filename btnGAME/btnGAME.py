e8123b99a2da8953c1f44acc50ac1537f610acfd
Fernando Manfredi <contact@acidhub.click>
2015-06-01 19:42:08 +0000

    Clear code

--- btnGAME/btnGAME.py
+++ btnGAME/btnGAME.py
@@ -23,7 +23,6 @@
 #
 
 import RPi.GPIO as GPIO
-#import wiringpi2
 from time import sleep
 import random
 from threading import Thread
@@ -31,7 +30,6 @@ import RPiMusic
 
 #Modo da GPIO seguindo a numeração BCM, ao inves da pinagem.
 GPIO.setmode(GPIO.BCM)
-#wiringpi2.wiringPiSetupGpio()
 
 class Blink(Thread):
     def __init__(self, parent):
@@ -43,11 +41,8 @@ class Blink(Thread):
             for led in [ self.parent.ledBlue, self.parent.ledGreen, self.parent.ledRed ]:
                 if self.parent.start:
                     GPIO.output(led, True)
-                    #wiringpi2.digitalWrite(led, 1)
                     sleep(random.uniform(0.5, 0.1))
-                    #wiringpi2.delay(random.uniform(500, 100))
                     GPIO.output(led, False)
-                    #wiringpi2.digitalWrite(led, 0)
                 else:
                     if not self.parent.running:
                         break
@@ -61,14 +56,6 @@ class Blink(Thread):
         # FIXME
         self.parent.pMusic.end()
         GPIO.cleanup()
-
-        #wiringpi2.pinMode(self.parent.ledBlue, 0)
-        #wiringpi2.pinMode(self.parent.ledGreen, 0)
-        #wiringpi2.pinMode(self.parent.ledRed, 0)
-
-        #wiringpi2.pinMode(self.parent.btnGreen, 0)
-        #wiringpi2.pinMode(self.parent.btnYellow, 0)
-        #wiringpi2.pinMode(self.parent.btnRed, 0)
 
 class Game:
     def __init__(self):
@@ -94,26 +81,14 @@ class Game:
         GPIO.setup(self.ledBlue, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.ledGreen, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.ledRed, GPIO.OUT, initial=GPIO.LOW)
-        #wiringpi2.pinMode(self.ledBlue, 1)
-        #wiringpi2.pinMode(self.ledGreen, 1)
-        #wiringpi2.pinMode(self.ledRed, 1)
 
         GPIO.setup(self.btnRed, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.btnYellow, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.btnGreen, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        #wiringpi2.pinMode(self.btnRed, 0)
-        #wiringpi2.pullUpDnControl(self.btnRed, 1)
-        #wiringpi2.pinMode(self.btnYellow, 0)
-        #wiringpi2.pullUpDnControl(self.btnYellow, 1)
-        #wiringpi2.pinMode(self.btnGreen, 0)
-        #wiringpi2.pullUpDnControl(self.btnGreen, 1)
 
         GPIO.add_event_detect(self.btnRed, GPIO.FALLING, callback=self._exit, bouncetime=300)
         GPIO.add_event_detect(self.btnYellow, GPIO.FALLING, callback=self.press_btnYellow, bouncetime=300)
         GPIO.add_event_detect(self.btnGreen, GPIO.FALLING, callback=self.press_btnGreen, bouncetime=1000)
-        #wiringpi2.wiringPiISR(self.btnRed, 0, self._exit())
-        #wiringpi2.wiringPiISR(self.btnYellow, 0, self.press_btnYellow())
-        #wiringpi2.wiringPiISR(self.btnGreen, 0, self.pess_btnGreen())
 
         self.thBlink = Blink(self)
 
@@ -144,7 +119,6 @@ class Game:
     verde estiver ligado, conita um ponto'''
     def press_btnYellow(self, obj):
         if GPIO.input(self.ledGreen):
-        #if wiringpi2.digitalRead(self.ledGreen):
             self.points +=1
 
             if self.points == 1:
@@ -183,4 +157,3 @@ class Game:
 if __name__ == '__main__':
     game = Game()
     game.main()
-
